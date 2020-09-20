982c4837c46915bcea09ad212b1310b3375685e7
Tor Gammelgård <tor.gammelgard@gmail.com>
2020-09-20 11:17:47 +0200

    Refactor

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:poker_trax_2/AddSession.dart';
+
+import 'SessionListView.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Poker Trax 2'),
+      ),
+      body: Center(
+          child: Column(children: [
+        Expanded(
+          child: SessionListView(),
+        ),
+      ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddSession()));
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void showSimpleFlushbar(BuildContext context) {
+    Flushbar(
+      // There is also a messageText property for when you want to
+      // use a Text widget and not just a simple String
+      message: 'Hello from a Flushbar',
+      // Even the button can be styled to your heart's content
+      mainButton: FlatButton(
+        child: Text(
+          'Click Me',
+          style: TextStyle(color: Theme.of(context).accentColor),
+        ),
+        onPressed: () {
+          print("Simple snackbar example");
+        },
+      ),
+      duration: Duration(seconds: 3),
+      // Show it with a cascading operator
+    )..show(context);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
-import 'package:poker_trax_2/AddSession.dart';
 
-import 'SessionListView.dart';
+import 'HomeScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,52 +15,5 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.pinkAccent,
         ),
         home: HomeScreen());
-  }
-
-  void show_Simple_Flushbar(BuildContext context) {
-    Flushbar(
-      // There is also a messageText property for when you want to
-      // use a Text widget and not just a simple String
-      message: 'Hello from a Flushbar',
-      // Even the button can be styled to your heart's content
-      mainButton: FlatButton(
-        child: Text(
-          'Click Me',
-          style: TextStyle(color: Theme.of(context).accentColor),
-        ),
-        onPressed: () {
-          print("Simple snackbar example");
-        },
-      ),
-      duration: Duration(seconds: 3),
-      // Show it with a cascading operator
-    )..show(context);
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Poker Trax 2'),
-      ),
-      body: Center(
-          child: Column(children: [
-            Expanded(
-              child: SessionListView(),
-            ),
-          ])),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => AddSession()));
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
   }
 }
